4d98855fc38966c3e3fc4462694a6b9f085e82bb
Steffina Jerald <steffina.jerald@gmail.com>
2025-07-16 21:41:50 -0400

    Initial commit with new files

---
+++ CodePath/ios101-project5-tumblr-starter-main/ios101-project5-tumblr/PostCell.swift
@@ -0,0 +1,23 @@
+//
+//  PostCell.swift
+//  ios101-project5-tumblr
+//
+//  Created by Steffina Jerald on 7/16/25.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
